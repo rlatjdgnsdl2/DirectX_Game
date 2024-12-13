e88d72dea6d5dbedb3cd60cf438cb82ed4394574
rlatjdgnsdl2 <rlatjdgnsdl2@gmail.com>
2024-12-13 17:27:38 +0900

    최종수정

--- DX_Game/EngineContents/ContentsCore.cpp
+++ DX_Game/EngineContents/ContentsCore.cpp
@@ -2,7 +2,7 @@
 #include "ContentsCore.h"
 
 
-// #define은 그냥 무조건 복붙
+
 CreateContentsCoreDefine(UContentsCore);
 //STDAPI_(__declspec(dllexport) INT_PTR) CreateContentsCore(std::shared_ptr<IContentsCore>& _Test)
 //{ 
@@ -24,13 +24,8 @@ UContentsCore::~UContentsCore()
 
 void UContentsCore::EngineStart(UEngineInitData& _Data)
 {
-	// mainwindow는 아무나 건들면 안된다.
-	// 넌 컨텐츠잖아 엔진이 관리하는 윈도우라는게 존재하는지도 몰라야한다.
-
 	_Data.WindowPos = { 100, 100 };
 	_Data.WindowSize = { 300, 300 };
-
-	// 윈도우 크기 지정
 }
 
 void UContentsCore::EngineTick(float _DeltaTime)
